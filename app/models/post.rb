103c9303c1787c0c1f367ec170cf3d5531ad0e65
Marisa You <marisayou2017@u.northwestern.edu>
2020-11-01 15:32:12 -0800

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ActiveRecord::Base
+    belongs_to :author
+
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+    validate :clickbaity_title
+
+    def clickbaity_title
+        clickbait = ["Won't Believe", "Secret", "Top", "Guess"]
+        if self.title && !clickbait.any? {|str| self.title.include?(str)}
+            errors.add(:title, "title is not clickbait-y enough")
+        end
+    end
+
 end
